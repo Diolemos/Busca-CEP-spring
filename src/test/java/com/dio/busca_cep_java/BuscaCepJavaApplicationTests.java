9b9ac67dad7b0590c9239ca76ead2421e8b48248
Dio Lemos <pedro.devsc@gmail.com>
2024-06-16 18:44:34 -0300

    first commit

---
+++ src/test/java/com/dio/busca_cep_java/BuscaCepJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dio.busca_cep_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuscaCepJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
